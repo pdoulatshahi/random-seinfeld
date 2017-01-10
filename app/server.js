d6066898800e419e8863f0c340bb2401c1c59541
pdoulatshahi <paul.doulatshahi@gmail.com>
2017-01-10 12:28:49 -0800

    Removing config package

--- app/server.js
+++ app/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
-const config = require('config');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
