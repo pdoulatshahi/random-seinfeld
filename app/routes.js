e4f2e339e509c671806d432c4fb08723e1367907
pdoulatshahi <paul.doulatshahi@gmail.com>
2017-01-11 10:48:28 -0800

    Fixing all seasons search

--- app/routes.js
+++ app/routes.js
@@ -9,14 +9,15 @@ module.exports = (app) => {
   });
 
   app.get('/randomEpisode', (req, res) => {
+    var seasons = req.param('s');
     var episodes = JSON.parse(fs.readFileSync('episodes.json', 'utf8'));
-    var seasons = req.param('s').map((num) => {
-      return parseInt(num)
-    });
     if (seasons) {
+      seasons = seasons.map((num) => {
+        return parseInt(num)
+      });
       episodes = episodes.filter((episode) => {
         return _.includes(seasons, episode.seasonNumber);
-      })
+      });
     }
     var episodeInfo = episodes[Math.floor(Math.random() * episodes.length)]
     res.render('random_episode', {episodeInfo})
