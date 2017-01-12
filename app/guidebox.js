16386cd8c160338256f64b29d2b1775dee0d34dc
pdoulatshahi <paul.doulatshahi@gmail.com>
2017-01-12 14:54:43 -0800

    Adding code for mobile link

--- app/guidebox.js
+++ app/guidebox.js
@@ -8,22 +8,21 @@ const Guidebox = new guidebox(GUIDEBOX_KEY);
 
 Guidebox.shows.episodes(2360, {include_links: true, limit: 180}).then((response) => {
   var episodes = [];
-  var arrOfEps = response.results;
   response.results.forEach((episode) => {
-    if (episode.subscription_web_sources.length > 0) {
-      episodes.push({
-        title: episode.title,
-        seasonNumber: episode.season_number,
-        episodeNumber: episode.episode_number,
-        overview: episode.overview,
-        firstAired: moment(episode.first_aired, "YYYY-MM-DD").format("MMM D, YYYY"),
-        imdbId: episode.imdb_id,
-        huluLink: episode.subscription_web_sources[0].link,
-        thumbnail: episode.thumbnail_208x117,
-        image: episode.thumbnail_608x342
-      })
+    newEpisode = {
+      title: episode.title,
+      seasonNumber: episode.season_number,
+      episodeNumber: episode.episode_number,
+      firstAired: moment(episode.first_aired, "YYYY-MM-DD").format("MMM D, YYYY"),
+      imdbId: episode.imdb_id,
+      thumbnail: episode.thumbnail_208x117,
+      image: episode.thumbnail_608x342
     }
-
+    if (episode.subscription_web_sources.length > 0) {
+      newEpisode.webLink = episode.subscription_web_sources[0].link;
+      newEpisode.mobileLink = episode.subscription_ios_sources[0].link;
+    }
+    episodes.push(newEpisode);
   })
   jsonfile.writeFile('episodes.json', episodes)
 })
--- public/js/script.js
+++ public/js/script.js
@@ -3,4 +3,11 @@ $(document).ready(() => {
     e.preventDefault();
     $('.one-season-checkbox').prop('checked', false);
   })
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $("#web-watch-link").hide();
+    $("#mobile-watch-link").show();
+  } else {
+    $("#web-watch-link").show();
+    $("#mobile-watch-link").hide();
+  }
 })
