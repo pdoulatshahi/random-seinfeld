2c795209b7782f9d298f31d584c5f9b01f9a54df
pdoulatshahi <paul.doulatshahi@gmail.com>
2017-01-12 22:45:13 -0800

    Editing analytics file

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -4,4 +4,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-90319924-1', 'auto');
-ga('send', 'pageview');
+
+$(document).ready(() => {
+  ga('send', 'pageview');
+})
