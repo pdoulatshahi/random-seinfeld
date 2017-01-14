684e685e3e499da0552da43481ca6c7cfee1dde0
pdoulatshahi <paul.doulatshahi@gmail.com>
2017-01-14 10:25:17 -0800

    DB seeded in local

--- app/seeds/episodes.js
+++ app/seeds/episodes.js
@@ -1,10 +1,15 @@
+require('./../config/config');
+
 const {mongoose} = require('./../db/mongoose');
 const {Episode} = require('./../models/episode');
 const fs = require('fs');
 
 var episodes = JSON.parse(fs.readFileSync('app/seeds/episodes.json', 'utf8'));
 
-episodes.forEach((episode) => {
-  ep = new Episode(episode);
-  ep.save();
-})
+// episodes.forEach((episode) => {
+//   ep = new Episode(episode);
+//   ep.save().then((ep) => {
+//   }, (e) => {
+//     res.status(400).send(e);
+//   })
+// })
