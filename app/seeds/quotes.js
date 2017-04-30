0df6927c474efd8672029f3f29a1ff2e20742e11
pdoulatshahi <paul.doulatshahi@gmail.com>
2017-04-29 20:34:35 -0700

    Fixing seed file

--- app/seeds/quotes.js
+++ app/seeds/quotes.js
@@ -8,48 +8,27 @@ const {mongoose} = require('./../db/mongoose');
 const {Episode} = require('./../models/episode');
 const {Quote} = require('./../models/quote');
 
-Episode.findById("587a8617fb3cf21c53924057").then((episode) => {
-  var imdbId = episode.imdbId;
-  var url = `http://www.imdb.com/title/${imdbId}/trivia?tab=qt&ref_=tt_trv_qu`;
-  request(url, (err, response, html) => {
-    var $ = cheerio.load(html);
-    var quoteElements = $(".sodatext");
-    console.log(quoteElements.length);
-    quoteElements.each(function(i, elem) {
-      var thisQuote = $(this).html();
-      var strippedQuote = striptags(thisQuote, '<span>');
-      var quote = new Quote({
-        text: strippedQuote,
-        _episode: episode._id
+
+Episode.find({}).then((episodes) => {
+  episodes.forEach((episode) => {
+    var imdbId = episode.imdbId;
+    if (imdbId.length > 0) {
+      var url = `http://www.imdb.com/title/${imdbId}/trivia?tab=qt&ref_=tt_trv_qu`;
+      request(url, (err, response, html) => {
+        var $ = cheerio.load(html);
+        $(".sodatext").each(function(i, elem) {
+          var thisQuote = $(this).html();
+          var strippedQuote = striptags(thisQuote, '<span>');
+          var quote = new Quote({
+            text: strippedQuote,
+            _episode: episode._id
+          })
+          quote.save().then((quote) => {
+            episode.quotes.push(quote._id);
+            episode.save();
+          })
+        })
       })
-      quote.save().then((quote) => {
-        episode.quotes.push(quote._id);
-        episode.save();
-      })
-    })
+    }
   })
 })
-
-// Episode.find({}).then((episodes) => {
-//   episodes.forEach((episode) => {
-//     var imdbId = episode.imdbId;
-//     if (imdbId.length > 0) {
-//       var url = `http://www.imdb.com/title/${imdbId}/trivia?tab=qt&ref_=tt_trv_qu`;
-//       request(url, (err, response, html) => {
-//         var $ = cheerio.load(html);
-//         $(".sodatext").each(function(i, elem) {
-//           var thisQuote = $(this).html();
-//           var strippedQuote = striptags(thisQuote, '<span>');
-//           var quote = new Quote({
-//             text: strippedQuote,
-//             _episode: episode._id
-//           })
-//           quote.save().then((quote) => {
-//             episode.quotes.push(quote._id);
-//             episode.save();
-//           })
-//         })
-//       })
-//     }
-//   })
-// })
